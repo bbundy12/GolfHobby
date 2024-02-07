66d3185371c96f9e761fe6c2393b6b3dfb0a4dd6
bbundy12 <brendanbundy1@gmail.com>
2024-02-07 14:36:14 -0700

    Add project files.

---
+++ GolfHobby/wwwroot/js/site.js
@@ -0,0 +1,39 @@
+﻿
+
+$(document).ready(function () {
+    $('#calculate').click(function () {
+        // Validation for positive number
+        var hours = $('#hours').val();
+        if (hours <= 0 || isNaN(hours)) {
+            alert('Please enter a positive number for hours.');
+            return false;
+        }
+
+        // Calculation
+        var hourlyRate = $('#hourlyRate').val();
+        var totalCharge = hours * hourlyRate;
+
+        // Display the result
+        $('#totalCharge').val(totalCharge.toFixed(2));
+    });
+});
+
+/*document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('calculate-total').addEventListener('click', function () {
+        // Validation for positive number
+        var hours = document.getElementById('hours-input').value;
+        if (hours <= 0 || isNaN(hours)) {
+            alert('Please enter a positive number for hours.');
+            return false;
+        }
+
+        // Calculation
+        var hourlyRate = document.getElementById('hourly-rate').value;
+        var totalCharge = hours * hourlyRate;
+
+        // Display the result
+        document.getElementById('total-charge').value = totalCharge.toFixed(2);
+    });
+});*/
+
+
